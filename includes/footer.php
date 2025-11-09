24fadb181321e227681c477f363e649862b69212
Josef Sika <207839954+kresadlo@users.noreply.github.com>
2025-11-09 20:35:29 +0100

    zaklad

---
+++ includes/footer.php
@@ -0,0 +1,3 @@
+<footer>
+    © <?php echo date('Y'); ?> | JukeDisc s. r. o.
+</footer>
---
+++ includes/header.php
@@ -0,0 +1,9 @@
+<header>
+    <ul>
+        <li><a href="eshop.php">E-shop</a></li>
+        <li><a href="example.php">example</a></li>
+        <li><a href="index.php"><img src="images/logo_wide.webp" alt="JukeDisc"></a></li>
+        <li><a href="kontakt.php">Kontakt</a></li>
+        <li><a href="onas.php">O nás</a></li>
+    </ul>
+</header>
